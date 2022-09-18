dde7cdaf96ba1e94446a5f8b7fdf64e9bb2711f9
Brazy <me@brazy.one>
2022-09-19 00:04:30 +0300

    Change bg on body

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Header from './components/Header';
+import Main from './components/Main';
 
-function App() {
+const App = () => {
   return (
-    <div className="p-6">
+    <div className="text-neutral-50 h-screen p-6">
       <Header />
+      <Main />
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,9 @@
-function Header() {
+const Header = () => {
   return (
     <div className="grid grid-cols-1 lg:grid-cols-3 gap-2">
-      <div className="grid text-2xl font-bold">
-        Apex Legends Memory Card
-      </div>
+      <div className="grid text-2xl font-bold">Apex Legends Memory Card</div>
       <div className="grid text-2xl lg:justify-center">
-        Rules: Click each card once
+        Try to Click Each Card Once
       </div>
       <div className="grid font-semibold lg:justify-center">
         <div>Best Score:</div>
@@ -13,6 +11,6 @@ function Header() {
       </div>
     </div>
   );
-}
+};
 
 export default Header;
